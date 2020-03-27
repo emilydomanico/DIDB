9957089e489e2879ad209df21f476ba10d6478a6
Emily Domanico <erdomanico@gmail.com>
2020-03-27 12:14:47 -0400

    Fixed () and {}

--- app.R
+++ app.R
@@ -7,7 +7,7 @@ library(readr)
 alldata <- read_csv("data.csv")
 
 ui <- fluidPage(
-  titlePanel("DI DB Thresholds_ TESTING"),
+  titlePanel("DI DB Thresholds"),
   setSliderColor(c(rep("DimGray",3)), c(1,2,3)),
   chooseSliderSkin("Flat"),
   sidebarLayout(
@@ -20,23 +20,17 @@ ui <- fluidPage(
       
       #Sliders to toggle sensitivity
     
-      sliderInput("Dim1", label = "Forcasting Error Threshold:", min = 0, 
-                  max = 100, value = 100),
+      sliderInput("Dim1", label = "Forcasting Error Threshold:", min = 0, max = 100, value = 100),
       p(""),
       hr(),
-      
   
-      sliderInput("Dim2", label = "Impact Threshold:", min = 0, 
-                  max = 100, value = 5),
+      sliderInput("Dim2", label = "Impact Threshold:", min = 0, max = 100, value = 5),
       p("How much impact is meaningful?"),
       hr(),
       
-      sliderInput("Dim3", label = "Burden Threshold:", min = 0, 
-                  max = 100, value = 5),
+      sliderInput("Dim3", label = "Burden Threshold:", min = 0, max = 100, value = 5),
       p("How much disproportionality is okay?")
-      
-
-  ),
+    ),
   
   mainPanel(
     column( width = 12,
@@ -44,10 +38,13 @@ ui <- fluidPage(
     ),
     column(width = 4,
     plotOutput(outputId= "impact_plot")
-    )
-    )
+    )#,
+    #column(width = 8,
+    #tableOutput("impact_table"))
+    #)
   )
-)
+))
+
 
 server <- function(input, output) {
   output$metric_plot <- renderPlot({
@@ -140,8 +137,6 @@ server <- function(input, output) {
       xlab("Population")
     print(metric_plot)
     
-    
-    
   })
   
   output$impact_plot <- renderPlot({
@@ -224,10 +219,82 @@ server <- function(input, output) {
     #ylim(-(dim2/0.01),dim2/0.01)
   print(impact_plot)
   
+  })
+  
+  output$impact_table <- renderTable({
+    #user imputs
+    #metric selected from selectInput dropdown
+    metric_filter <- input$metric 
+    # percentage threshold set by slider
+    dim1 <- input$Dim1*0.01
+    dim2 <- input$Dim2*0.01
+    dim3 <- input$Dim3*0.01
+    
+    data <- alldata %>%
+      filter(metric == metric_filter)
+    #the hyphen causes some problems when if becomes a column name, so replace the value with an underscore to clean up code further down
+    data[data=="no-build"] <- "no_build"
+    data[data=="Low-income"] <- "Low_income"
+    data[data=="Non-low-income"] <- "Non_low_income"
+    data[data=="Non-minority"] <- "Non_minority"
+    ##Calculate error and scale by dim1. Use result to calculate upper and lower bounds for build & no-build scenarios
+    ##Calculate impact and scale by dim2 for each population
+    
+    ## note: error_aug from alldata used as error, includes 10% confidence interval and zscore
+    
+    ## To find if building will trigger an impact
+    ## filter by population, calculate differences between build and no build (delta), determine impact from scaled delta (by dim2), and bring into common dataframe
+    
+    
+    data <- data %>%
+      spread( scenario, model_result, drop = TRUE)
+    data <- data %>%
+      #step 1 from spreadsheet
+      mutate(delta = build - no_build) %>%
+      #scaling delta by dim2 slider
+      #note: this use of dim2 could be wrong/ not useful
+      mutate(delta_scaled = delta*dim2) %>%
+      mutate(error_b = build*error_aug) %>%
+      mutate(error_nb =no_build*error_aug) %>%
+      #scaling error by dim1 slider
+      mutate(error_scaled_b = error_b*dim1) %>%
+      mutate(error_scaled_nb =error_nb*dim1) %>%
+      mutate(LB_b = build-error_scaled_b) %>%
+      mutate(UB_b = build+error_scaled_b) %>%
+      mutate(LB_nb = no_build - error_scaled_nb)%>%
+      mutate(UB_nb = no_build + error_scaled_nb) %>%
+      
+      #step 2 from spreadsheet
+      #Impact option 1a: impact as calculated in the spreadsheet
+      mutate(impact_original = case_when (abs(delta)> abs(error_nb) ~ "Impact",
+                                          TRUE ~ "No Impact")) %>%
+      #Impact option 1b: impact as calculated in speadsheet + accounting for threshold sliders
+      mutate(impact_scaled = case_when (abs(delta_scaled) > abs(error_scaled_nb) ~ "Impact",
+                                        TRUE ~ "No Imapact")) %>%
+      
+      #Impact option 2: impact as absolute change (note 0 to 100 slider not appropriate for all metrics)
+      mutate(impact_absolute = case_when ( abs(delta) > dim2/.01 ~"Impact",
+                                           TRUE ~ "No Impact")) %>%
+      #Impact option 3: impact as percentage point change with threshold set by slider 2
+      mutate(percent_change= round(delta/no_build*100, digits = 1)) %>%
+      mutate(impact_perctdiff = case_when (abs(percent_change) > dim2/.01 ~ "Impact",
+                                           TRUE ~ "No Impact"))
+    
+    
+    #make an impact table, bring together all impact option by population in a table
+    impact_table <- data %>%
+      select( population,delta,percent_change, starts_with("impact"))%>%
+      mutate( poptype = case_when (str_detect(population, ".inority") ~ "m",
+                                   str_detect(population, ".ncome") ~ "i",
+                                   TRUE ~ "NA")) %>%
+      select(poptype, population, delta, percent_change, impact_original, impact_scaled, impact_absolute, impact_perctdiff) %>%
+      # control order of entries
+      arrange(factor(population, levels = c("Low_income","Non_low_income", "Minority","Non_minority")))
+    print(impact_table)
+  })
   
   
   
-      })
 
 }
 
